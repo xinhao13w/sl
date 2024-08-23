1466e3fb088c0ea5caa3ceb224ce882761cad3bf
xinhao13w <xinhao13@126.com>
2024-08-23 11:09:28 +0800

    Site updated: 2024-08-23 11:09:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/22/steam安装教程/","2024/08/20/本站的第一篇文章/"];function toRandomPost(){
+var posts=["posts/58568.html","posts/577.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
