51e5fc9ad7192d8b45cfc111b51286e6913e7092
xinhao13w <xinhao13@126.com>
2024-08-27 19:59:21 +0800

    Site updated: 2024-08-27 19:59:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/577.html","posts/37845.html","posts/58568.html"];function toRandomPost(){
+var posts=["posts/37845.html","posts/58568.html","posts/577.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
